b8a8804f5f8bbc1789c2a970a64c7e5a2854d57c
Hareesh <hareesh@cse.iitb.ac.in>
2019-01-05 18:02:57 +0530

    Moving to Lab server

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ MQTT_HOST="10.129.149.9"
 MQTT_PORT=1883
 MQTT_TOPIC="nodemcu/h10/dht/temp_h10"
 
-IP=""
-USERNAME=""
-PASSWORD=""
-DB=""
+IP="10.129.149.9"
+USERNAME="reader"
+PASSWORD="reader"
+DB="temperaturedb"
--- mqtt_to_mysql.py
+++ mqtt_to_mysql.py
@@ -1,32 +1,38 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt
 import logging
+from logging.config import fileConfig
+
 import time
 from datetime import datetime
 from MySQLdb import *
 import sys
-from db import insert_to_db
 from config import *
 
 
 
 
 
-def insert_to_mysql(room_name,ts,node_id,temp,humidity,volt):
-        con = connect("10.129.23.161","writer","datapool","cooling")
+def insert_to_mysql(ts, sensor_id, temperature, humidity, battery):
+        try:
+            con = connect(IP, USERNAME, PASSWORD, DB)
+        except:
+            logging.error("Execution failed -- Rollback in progress..")
+
         cur = con.cursor()
 
-        sql  = "insert into temperature_analysis(room_name,timestamp,node_id,temperature,humidity,voltage)  values('%s','%s','%d','%f','%f','%f')" %(room_name,ts,node_id,temp,humidity,volt)
-        # print sql
+        sql  = "insert into temp_data(time_stamp, sensor_id, temp, humidity, battery) values('%f','%s','%f','%f','%f')" %(ts, sensor_id, temperature, humidity, battery)
+        print sql
         try:
-                # print "Executing sql"
-                cur.execute(sql)
-                # print "Executed sql"
+                print "Executing sql"
+                cur.execute(sql+""+ds)
+                print "Executed sql"
                 con.commit()
-                # print "Insertion done -- Done"
+                print "Insertion done -- Done"
         except:
                 con.rollback()
-                print "Execution failed -- Rollback in progress.."
+                logging.error("Execution failed -- Rollback in progress..")
+                logging.error(ts, sensor_id, temperature, humidity, battery)
         con.close()
 
 
@@ -36,19 +42,19 @@ def get_mqtt_msg():
 
     def on_connect(client, userdata, flags, rc):
         client.subscribe(MQTT_TOPIC)
-        print "Subscibed to "+MQTT_TOPIC
+        logger.info("Subscibed to "+MQTT_TOPIC)
 
     def on_message(client, userdata, msg):
       print datetime.now().strftime('%H:%M:%S  : ')+ "Recived "+  msg.payload
       message=msg.payload.split(",")
-      node_id=int(message[0])
+      sensor_id=message[0]
       temp=float(message[1])
       humidity=float(message[2])
       volt=float(message[3])
-      ts=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-      print(ts,node_id,temp,humidity,volt)
+      ts=int(time.time())
+      insert_to_mysql(ts, sensor_id, temp, humidity, volt)
 
-    client = mqtt.Client("Temperature Variance")
+    client = mqtt.Client("Temperature Collection -- Hareesh")
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(MQTT_HOST, MQTT_PORT, 60)
@@ -57,4 +63,8 @@ def get_mqtt_msg():
 
 
 if __name__=="__main__":
+    fileConfig('config.ini')
+    logger=logging.getLogger()
+    logger.info('Starting Temperature Logging')
+
     get_mqtt_msg()
