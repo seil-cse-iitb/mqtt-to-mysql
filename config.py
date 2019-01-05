64f8be5ad6e8f24ec8e0c2d6144d05b30fbc00d2
Hareesh <hareesh@cse.iitb.ac.in>
2019-01-05 16:25:59 +0530

    Starting edits

---
+++ config.py
@@ -0,0 +1,9 @@
+#configuration
+MQTT_HOST="10.129.149.9"
+MQTT_PORT=1883
+MQTT_TOPIC="nodemcu/h10/dht/temp_h10"
+
+IP=""
+USERNAME=""
+PASSWORD=""
+DB=""
--- mqtt_to_mysql.py
+++ mqtt_to_mysql.py
@@ -1,20 +1,42 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt
+import logging
 import time
 from datetime import datetime
 from MySQLdb import *
 import sys
 from db import insert_to_db
+from config import *
 
-MQTT_HOST="10.129.23.41"
-MQTT_PORT=1883
-MQTT_TOPIC="nodemcu/kresit/dht/"
-room_name=""
+
+
+
+
+def insert_to_mysql(room_name,ts,node_id,temp,humidity,volt):
+        con = connect("10.129.23.161","writer","datapool","cooling")
+        cur = con.cursor()
+
+        sql  = "insert into temperature_analysis(room_name,timestamp,node_id,temperature,humidity,voltage)  values('%s','%s','%d','%f','%f','%f')" %(room_name,ts,node_id,temp,humidity,volt)
+        # print sql
+        try:
+                # print "Executing sql"
+                cur.execute(sql)
+                # print "Executed sql"
+                con.commit()
+                # print "Insertion done -- Done"
+        except:
+                con.rollback()
+                print "Execution failed -- Rollback in progress.."
+        con.close()
+
+
+
+
 def get_mqtt_msg():
 
     def on_connect(client, userdata, flags, rc):
-        client.subscribe(MQTT_TOPIC+room_name)
-        print "Subscibed to "+MQTT_TOPIC+room_name
+        client.subscribe(MQTT_TOPIC)
+        print "Subscibed to "+MQTT_TOPIC
 
     def on_message(client, userdata, msg):
       print datetime.now().strftime('%H:%M:%S  : ')+ "Recived "+  msg.payload
@@ -24,9 +46,9 @@ def get_mqtt_msg():
       humidity=float(message[2])
       volt=float(message[3])
       ts=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-      insert_to_db(room_name,ts,node_id,temp,humidity,volt)
+      print(ts,node_id,temp,humidity,volt)
 
-    client = mqtt.Client("LH Data -- Parth")
+    client = mqtt.Client("Temperature Variance")
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(MQTT_HOST, MQTT_PORT, 60)
@@ -35,5 +57,4 @@ def get_mqtt_msg():
 
 
 if __name__=="__main__":
-    room_name=sys.argv[1]
     get_mqtt_msg()
